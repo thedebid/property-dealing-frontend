0a3f69eaabe964795cc577415e66fb79ae864ca8
Debid Magar <linkwithdm@gmail.com>
2020-07-18 21:51:41 +0545

    add property component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { PropertyCardComponent } from './property/property-card/property-card.co
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     PropertyCardComponent,
     PropertyListComponent,
+    AddPropertyComponent,
   ],
   imports: [BrowserModule, HttpClientModule],
   providers: [HousingService],
